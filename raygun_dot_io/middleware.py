182fd5c969f6d5fb32c1e5823ba724b69b87a8f8
Unai Zalakain <unai@gisa-elkartea.org>
2016-09-06 16:46:08 +0200

    Get the request body directly through request.read()
    request.body will fail if a read hasn't started.

--- raygun_dot_io/middleware.py
+++ raygun_dot_io/middleware.py
@@ -65,7 +65,6 @@ class RaygunException(object):
             if not k.startswith('wsgi'):
                 _headers[k] = v
 
-        rawData = (self.request.body if hasattr(self.request, 'body') else self.request.raw_post_data).decode('utf-8')
         return {
             'hostName': self.request.get_host(),
             'url': self.request.path,
@@ -74,7 +73,7 @@ class RaygunException(object):
             'queryString': dict((key, self.request.GET[key]) for key in self.request.GET),
             'form': dict((key, self.request.POST[key]) for key in self.request.POST),
             'headers': _headers,
-            'rawData': rawData,
+            'rawData': self.request.read().decode('utf-8'),
         }
 
     def _getClientData(self):
